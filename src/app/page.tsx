a9e0493b36b9cb9a79161af92500b37faaa521b9
Greg <gregvermaak@icloud.com>
2023-06-08 13:09:23 +1200

    homepage complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,47 @@
+import LargeHeading from "@/components/ui/LargeHeading"
+import Paragraph from "@/components/ui/Paragraph"
+import type { Metadata } from "next"
+import Image from "next/image"
+import Link from "next/link"
+
+export const metadata: Metadata = {
+  title: "Similarity API | Home",
+  description: "Free & Open Source Similarity API",
+}
+
 export default function Home() {
-  return <main className="bg-red-500"></main>
+  return (
+    <div className="relative flex h-screen items-center justify-center overflow-x-hidden">
+      <div className="container mx-auto h-full w-full max-w-7xl pt-32">
+        <div className="flex h-full flex-col items-center justify-start gap-6 lg:items-start lg:justify-center">
+          <LargeHeading
+            size="lg"
+            className="three-d text-black dark:text-light-gold">
+            Easily determine <br /> text similarity.
+          </LargeHeading>
+          <Paragraph className="max-w-xl lg:text-left">
+            With the text similarity API, you can easily determine the
+            similarity between two pieces of text with a free{" "}
+            <Link
+              href="/login"
+              className="text-black underline underline-offset-2 dark:text-light-gold">
+              API key
+            </Link>
+            .
+          </Paragraph>
+          <div className="relative aspect-square w-full max-w-lg lg:absolute lg:left-1/2 lg:max-w-3xl">
+            <Image
+              priority
+              className="image-shadow"
+              quality={100}
+              style={{ objectFit: "contain" }}
+              fill
+              src="/typewriter.png"
+              alt="Typewriter"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
